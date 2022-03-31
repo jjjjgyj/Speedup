9d048a6203fac4f42e899ca5597dd7ef3da2ad3f
JJJJ <yunjiagao27@gmail.com>
2022-03-31 11:48:19 -0700

    Checking in code for Speedup

---
+++ src/com/company/calculateSum.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class calculateSum implements Runnable{
+    private int portion;
+
+    public calculateSum(int portion){
+        this.portion = portion;
+    }
+
+    @Override
+    public void run() {
+        long sum = 0;
+        int start = portion * Main.portionSize;
+        int end = portion * Main.portionSize + Main.portionSize;
+
+        for(int i = start; i < end; i++){
+            sum += Main.values[i];
+        }
+
+        Main.portionSum[portion] = sum;
+
+    }
+}
